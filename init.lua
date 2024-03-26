3094d3b2235c615f4aa42cfd6c7206305062218f
Jakub Reroń <kubareron@gmail.com>
2024-03-26 21:22:16 +0900

    Revert "feat: add vim.opt options"
    This reverts commit bab3049b1f465724207e61efc7372a1399a9cde3.

--- init.lua
+++ init.lua
@@ -1,4 +1,37 @@
 --[[
+
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||   KICKSTART.NVIM   ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
+
+What is Kickstart?
+
+  Kickstart.nvim is *not* a distribution.
+
+  Kickstart.nvim is a starting point for your own configuration.
+    The goal is that you can read every line of code, top-to-bottom, understand
+    what your configuration is doing, and modify it to suit your needs.
+
+    Once you've done that, you can start exploring, configuring and tinkering to
+    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
+    or immediately breaking it into modular pieces. It's up to you!
+
     If you don't know anything about Lua, I recommend taking some time to read through
     a guide. One possible example which will only take 10-15 minutes:
       - https://learnxinyminutes.com/docs/lua/
@@ -8,23 +41,68 @@
     - :help lua-guide
     - (or HTML version): https://neovim.io/doc/user/lua-guide.html
 
+Kickstart Guide:
+
+  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+
+    If you don't know what this means, type the following:
+      - <escape key>
+      - :
+      - Tutor
+      - <enter key>
+
+    (If you already know the Neovim basics, you can skip this step.)
+
+  Once you've completed that, you can continue working through **AND READING** the rest
+  of the kickstart init.lua.
+
+  Next, run AND READ `:help`.
+    This will open up a help window with some basic information
+    about reading, navigating and searching the builtin help documentation.
+
+    This should be the first place you go to look when you're stuck or confused
+    with something. It's one of my favorite Neovim features.
+
     MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
     which is very useful when you're not exactly sure of what you're looking for.
 
+  I have left several `:help X` comments throughout the init.lua
+    These are hints about where to find more information about the relevant settings,
+    plugins or Neovim features used in Kickstart.
+
+   NOTE: Look for lines like this
+
+    Throughout the file. These are for you, the reader, to help you understand what is happening.
+    Feel free to delete them once you know what you're doing, but they should serve as a guide
+    for when you are first encountering a few different constructs in your Neovim config.
+
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+
+I hope you enjoy your Neovim journey,
+- TJ
+
+P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
 -- Set <space> as the leader key
+-- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false
 
--- Show numbers
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
 vim.opt.number = true
-vim.opt.relativenumber = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -75,64 +153,12 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
-vim.opt.sidescrolloff = 10
 
--- dictionaries
-vim.opt.dictionary = '/usr/share/dict/words'
-vim.opt.thesaurus = '~/.config/nvim/mthesaur.txt'
-
--- creates a backup file
-vim.opt.backup = false
-
--- so that `` is visible in markdown files
-vim.opt.conceallevel = 0
-
--- pop up menu height
-vim.opt.pumheight = 10
-
--- creates a swapfile
-vim.opt.swapfile = false
-
--- set the title of window to the value of the titlestring
-vim.opt.title = true
-
--- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-vim.opt.writebackup = false
-
--- Don't redraw while executing macros (good performance config)
-vim.opt.lazyredraw = true
-
--- show status line always and ONLY the last window
-vim.opt.laststatus = 3
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-
--- set linebreak on very long lines
-vim.opt.linebreak = true
-
--- linebreak on 500 characters
-vim.opt.textwidth = 500
-
--- show indicator on 80th column
-vim.opt.colorcolumn = '80'
-
--- format the winbar text
-vim.opt.winbar = '%=%m %f'
-
--- disable spellchecking for asian characters (VIM algorithm does not support it)
-vim.opt.spelllang:append 'cjk'
-
--- don't show redundant messages from ins-completion-menu
-vim.opt.shortmess:append 'c'
-
--- don't show the default intro message
-vim.opt.shortmess:append 'I'
-vim.opt.whichwrap:append '<,>,[,],h,l'
-
--- match git conflict markers with %
-vim.b.match_words = '<<<<<<<:=======:>>>>>>>'
-
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
