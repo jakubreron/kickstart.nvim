226221b64f6e03d8008776653070d9b14e91846d
Jakub Reroń <kubareron@gmail.com>
2024-03-27 19:13:12 +0900

    cleanup: remove console.debug from js-ts

--- lua/custom/keymaps/javascript-and-typescript.lua
+++ lua/custom/keymaps/javascript-and-typescript.lua
@@ -1,10 +1,7 @@
 local console_log = "console.log('%c', 'font-size: 24px; color: skyblue;')<ESC>02f'i "
-local console_debug = "console.debug()<ESC>0f(a"
 
 local M = {
   c = {
-    D = { "O" .. console_debug, "console.debug above" },
-    d = { "o" .. console_debug, "console.debug below" },
     L = { "O" .. console_log, "console.log above" },
     l = { "o" .. console_log, "console.log below" },
     p = {
