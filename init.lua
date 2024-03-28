483cd5f70bd34409c1bdea56006c1f2a2d823c03
Jakub Reroń <kubareron@gmail.com>
2024-03-28 10:35:50 +0900

    config(telescope): use ivy theme

--- init.lua
+++ init.lua
@@ -359,7 +359,7 @@ require('lazy').setup({
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
+            require('telescope.themes').get_ivy(),
           },
         },
       }
