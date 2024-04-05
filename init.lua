44cd3c312c3b78f4601d685c33283e46c57e5748
Jakub Reroń <kubareron@gmail.com>
2024-04-05 15:28:44 +0900

    fix(live_grep): get_ivy theme

--- init.lua
+++ init.lua
@@ -330,7 +330,7 @@ require('lazy').setup({
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
       vim.keymap.set('n', '<leader>s/', function()
-        telescope_theme_wrapper(builtin.live_grep {
+        builtin.live_grep(require('telescope.themes').get_ivy {
           grep_open_files = true,
           prompt_title = 'Live Grep in Open Files',
         })
