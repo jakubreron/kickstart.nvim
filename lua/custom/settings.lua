3c8693c127c615cf86c93a3ec659c4beff3c20d3
Jakub Reroń <kubareron@gmail.com>
2024-04-05 14:42:29 +0900

    config(settings): simplify the settings and add diagnostic config

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -1,20 +1,12 @@
-local options = {
-  dictionary = '/usr/share/dict/words',
-  backup = false, -- creates a backup file
-  completeopt = { 'menuone', 'noselect' },
-  conceallevel = 0, -- so that `` is visible in markdown files
-  pumheight = 10, -- pop up menu height
-  swapfile = false, -- creates a swapfile
-  termguicolors = true, -- set term gui colors (most terminals support this)
-  title = true, -- set the title of window to the value of the titlestring
-  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  laststatus = 3,
-  lazyredraw = true, -- Don't redraw while executing macros (good performance config)
-  linebreak = true, -- set linebreak on very long lines
-  textwidth = 500, -- linebreak on 500 characters
-  colorcolumn = '80',
-  -- winbar = '|%t|', -- file title
-}
+-- vim.opt.dictionary = '/usr/share/dict/words' -- ctrl-x ctrl-k word suggestion
+vim.opt.backup = false -- creates a backup file
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.title = true -- set the title of window to the value of the titlestring
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
+vim.opt.lazyredraw = true -- Don't redraw while executing macros (good performance config)
+vim.opt.colorcolumn = '80'
+-- vim.opt.winbar = '|%t|' -- file title
 
 vim.opt.spelllang:append 'cjk' -- disable spellchecking for asian characters (VIM algorithm does not support it)
 vim.opt.shortmess:append 'c' -- don't show redundant messages from ins-completion-menu
@@ -28,6 +20,10 @@ vim.filetype.add {
   },
 }
 
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
+vim.diagnostic.config {
+  virtual_text = false,
+  float = {
+    border = 'rounded',
+    source = 'always',
+  },
+}
