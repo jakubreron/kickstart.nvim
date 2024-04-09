cc92b114a53db9ef1ade28ba96cecfdbad03f3a2
Jakub Reroń <kubareron@gmail.com>
2024-04-09 13:44:46 +0900

    config(keymaps): track custom session improvements

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -135,7 +135,7 @@ return {
     cmd = { 'Obsession' },
     keys = {
       { '<leader>ot', '<cmd>Obsession<cr>', desc = '[t]rack session' },
-      { '<leader>oT', ':Obsession Session-', desc = '[T]rack custom session' },
+      { '<leader>oT', ':Obsession Session-.vim<Left><Left><Left><Left>', desc = '[T]rack custom session' },
       { '<leader>os', '<cmd>source Session.vim<cr>', desc = '[s]ource session' },
       { '<leader>oS', ':source Session-', desc = '[S]ource custom session' },
     },
