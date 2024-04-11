63bfb1b8c72150a481b7088da99867ca00e55855
Jakub Reroń <kubareron@gmail.com>
2024-04-11 16:23:27 +0900

    feat: git conflict plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -210,6 +210,52 @@ return {
   --   config = true,
   -- },
 
+  -- co — choose ours
+  -- ct — choose theirs
+  -- cb — choose both
+  -- c0 — choose none
+  -- ]x — move to previous conflict
+  -- [x — move to next conflict
+  {
+    'akinsho/git-conflict.nvim',
+    version = '*',
+    config = function()
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'GitConflictDetected',
+        callback = function()
+          vim.keymap.set('n', '<C-q>', '<cmd>GitConflictListQf<cr>', { desc = 'Conflicts quicklist' })
+          vim.notify('Conflict detected in ' .. vim.fn.expand '<afile>')
+          require('conform').setup { format_on_save = false }
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'GitConflictResolved',
+        callback = function()
+          vim.keymap.set('n', '<C-q>', '<C-v>')
+          vim.notify('Conflict resolved in ' .. vim.fn.expand '<afile>')
+          require('conform').setup {
+            format_on_save = {
+              timeout_ms = 500,
+              lsp_fallback = false,
+            },
+          }
+        end,
+      })
+
+      require('git-conflict').setup {
+        default_mappings = true, -- disable buffer local mapping created by this plugin
+        default_commands = true, -- disable commands created by this plugin
+        disable_diagnostics = true, -- This will disable the diagnostics in a buffer whilst it is conflicted
+        list_opener = 'copen', -- command or function to open the conflicts list
+        highlights = { -- They must have background color, otherwise the default color will be used
+          incoming = 'DiffAdd',
+          current = 'DiffText',
+        },
+      }
+    end,
+  },
+
   {
     'nvim-tree/nvim-tree.lua',
     version = '*',
