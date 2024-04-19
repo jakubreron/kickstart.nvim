28a1cdc5ed5b33695737a3f88e9c26f23830ea3f
Jakub Reroń <kubareron@gmail.com>
2024-04-19 18:27:07 +0900

    feat(typescript): re-format with conform after running TS commands

--- lua/custom/keymaps/ftplugin/which_key/typescript.lua
+++ lua/custom/keymaps/ftplugin/which_key/typescript.lua
@@ -1,25 +1,35 @@
+local action_with_formatting = function(command)
+  return function()
+    vim.cmd(command)
+
+    vim.defer_fn(function()
+      require('conform').format { async = true, lsp_fallback = false }
+    end, 200)
+  end
+end
+
 local M = {
   f = {
-    r = { '<cmd>TSToolsRenameFile<CR>', '[r]ename' },
-    R = { '<cmd>TSToolsFileReferences<CR>', '[R]eferences' },
+    r = { action_with_formatting 'TSToolsRenameFile', '[r]ename' },
+    R = { action_with_formatting 'TSToolsFileReferences', '[R]eferences' },
   },
   l = {
     i = {
       name = '[i]mports',
-      o = { '<cmd>TSToolsOrganizeImports<CR>', '[o]rganize' },
-      s = { '<cmd>TSToolsSortImports<CR>', '[s]ort' },
-      r = { '<cmd>TSToolsRemoveUnusedImports<CR>', '[r]emove unused' },
-      a = { '<cmd>TSToolsAddMissingImports<CR>', '[a]dd missing' },
-    }, -- default is :LspInfo
+      o = { action_with_formatting 'TSToolsOrganizeImports', '[o]rganize' },
+      s = { action_with_formatting 'TSToolsSortImports', '[s]ort' },
+      r = { action_with_formatting 'TSToolsRemoveUnusedImports', '[r]emove unused' },
+      a = { action_with_formatting 'TSToolsAddMissingImports', '[a]dd missing' },
+    },
     o = {
       name = '[o]rganize',
-      r = { '<cmd>TSToolsRemoveUnused<CR>', '[r]emove unused statements' },
-      f = { '<cmd>TSToolsFixAll<CR>', '[f]ix fixable errors' },
-    }, -- [o]rganize
+      r = { action_with_formatting 'TSToolsRemoveUnused', '[r]emove unused statements' },
+      f = { action_with_formatting 'TSToolsFixAll', '[f]ix fixable errors' },
+    },
   },
 }
 
-vim.keymap.set('n', 'gd', '<cmd>TSToolsGoToSourceDefinition<CR>', {
+vim.keymap.set('n', 'gd', '<cmd>TSToolsGoToSourceDefinition<cr>', {
   desc = '[g]oto [d]efiniton',
   buffer = vim.api.nvim_get_current_buf(),
 })
