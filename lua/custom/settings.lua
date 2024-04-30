b22e763852e3fe94ceb282b64d035b0c2a004683
Jakub Reroń <kubareron@gmail.com>
2024-04-30 18:11:43 +0900

    config: enable colorcolumn

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -5,7 +5,7 @@ vim.opt.swapfile = false -- creates a swapfile
 vim.opt.title = true -- set the title of window to the value of the titlestring
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
 vim.opt.lazyredraw = true -- Don't redraw while executing macros (good performance config)
--- vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = '80'
 vim.opt.laststatus = 3
 -- vim.opt.winbar = '|%t|' -- file title
 
