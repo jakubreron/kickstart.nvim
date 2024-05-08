662a64bc2ac2a6f1ed385daa3465d3507d0c09f0
Jakub Reroń <kubareron@gmail.com>
2024-05-08 19:51:06 +0900

    config: save on tmux pane switch, disable navigator when zoomed

--- lua/custom/plugins/settings/vars.lua
+++ lua/custom/plugins/settings/vars.lua
@@ -1,6 +1,3 @@
--- match git conflict markers with %
-vim.b.match_words = '<<<<<<<:=======:>>>>>>>'
-
 -- use var for vimwiki home dir
 vim.g.vimwiki_list = {
   {
@@ -12,6 +9,8 @@ vim.g.vimwiki_list = {
 
 -- do not wrap tmux
 vim.g.tmux_navigator_no_wrap = 1
+vim.g.tmux_navigator_disable_when_zoomed = 1
+vim.g.tmux_navigator_save_on_switch = 2
 
 -- remove unnecessary unimpaired mappings
 vim.g.nremap = {
