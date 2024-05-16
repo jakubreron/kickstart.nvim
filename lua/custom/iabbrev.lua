2c957eb2e0218895d6bc8a8022d8cc3c7b68609e
Jakub Reroń <kubareron@gmail.com>
2024-05-16 21:18:53 +0900

    config(iabbrv): add TODO comments

--- lua/custom/iabbrev.lua
+++ lua/custom/iabbrev.lua
@@ -2,13 +2,20 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*.js', '*.ts', '*.vue', '*.jsx', '*.tsx' },
   callback = function()
     vim.cmd [[
-     function! Eatchar(pat)
-       let char = nr2char(getchar(0))
-       return (char =~ a:pat) ? '' : char
-     endfunction
+     func Eatchar(pat)
+        let c = nr2char(getchar(0))
+        return (c =~ a:pat) ? '' : c
+     endfunc
 
-     iabbrev cl console.log()<Left><c-r>=Eatchar('\m\s\<bar>/')<cr>
-     iabbrev cd console.debug()<Left><c-r>=Eatchar('\m\s\<bar>/')<cr>
+     iabbrev cl console.log()<Left><C-R>=Eatchar('\s')<CR>
+     iabbrev cd console.debug()<Left><C-R>=Eatchar('\s')<CR>
    ]]
   end,
 })
+
+vim.cmd 'iabbrev PE PERF:<ESC>gccA'
+vim.cmd 'iabbrev HK HACK:<ESC>gccA'
+vim.cmd 'iabbrev TD TODO: @Jakub<ESC>gccA'
+vim.cmd 'iabbrev NO NOTE:<ESC>gccA'
+vim.cmd 'iabbrev WA WARNING:<ESC>gccA'
+vim.cmd 'iabbrev FX FIX:<ESC>gccA'
