e90b8d91cdf9ceac3b946dd7bcf38ed5deebf28b
Jakub Reroń <kubareron@gmail.com>
2024-06-27 13:29:44 +0900

    perf: replace nvim-ts-context-commentstring with lightweight ts-comments

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -268,19 +268,10 @@ return {
   },
 
   {
-    'JoosepAlviste/nvim-ts-context-commentstring',
+    'folke/ts-comments.nvim',
     lazy = true,
     ft = { 'typescriptreact', 'vue' },
-    config = function()
-      require('ts_context_commentstring').setup {
-        enable_autocmd = false,
-      }
-
-      local get_option = vim.filetype.get_option
-      vim.filetype.get_option = function(filetype, option)
-        return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
-      end
-    end,
+    event = 'VeryLazy',
   },
 
   {
