b22389fb6b0072cc0938ad83b3c0276f83462235
Jakub Reroń <kubareron@gmail.com>
2024-09-11 11:10:33 +0900

    config: oil timeout 5000 -> 50000

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -292,12 +292,13 @@ return {
         ['<C-l>'] = false, -- refresh
         ['<C-h>'] = false, -- horizontal split
         ['<C-s>'] = false, -- vertical split
+        -- ['~'] = false, -- cwd to current dir (cannot change the case)
       },
       view_options = {
         show_hidden = true,
       },
       lsp_file_methods = {
-        timeout_ms = 5000,
+        timeout_ms = 50000,
         autosave_changes = true,
       },
     },
