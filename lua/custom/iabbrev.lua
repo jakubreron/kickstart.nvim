16a8ec52ed9823e1208375632973c5f63fc43bc7
Jakub Reroń <kubareron@gmail.com>
2024-10-04 18:01:32 +0900

    fix(iabbrev)

--- lua/custom/iabbrev.lua
+++ lua/custom/iabbrev.lua
@@ -1,14 +1,14 @@
 vim.cmd [[
-  iabbrev PP PERF:<ESC>gcc$Fba
-  iabbrev HH HACK:<ESC>gcc$Fba
-  iabbrev TT TODO: @Jakub<ESC>gcc$Fba
-  iabbrev NN NOTE:<ESC>gcc$Fba
-  iabbrev WW WARNING:<ESC>gcc$Fba
-  iabbrev FF FIX:<ESC>gcc$Fba
+  iabbrev PP PERF:<ESC>gcc^f:a
+  iabbrev HH HACK:<ESC>gcc^f:a
+  iabbrev TT TODO: @Jakub<ESC>gcc^fba
+  iabbrev NN NOTE:<ESC>gcc^f:a
+  iabbrev WW WARNING:<ESC>gcc^f:a
+  iabbrev FF FIX:<ESC>gcc^f:a
 
   iabbrev CSSP content: '';<ESC>odisplay: block;<ESC>oposition: absolute;<ESC>otop: 0;<ESC>oright: 0;<ESC>o
   
-  iabbrev iR import * as React from 'react'<ESC>xa
+  iabbrev iR import * as React from 'react'<ESC>a
 
   iabbrev tsi @ts-ignore<ESC>gcc
 
