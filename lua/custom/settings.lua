5a9320c02b4cb17ea26d8cba879ddd03a6a87c8d
Jakub Reroń <kubareron@gmail.com>
2024-12-25 14:06:12 +0900

    config: restore more nvim & kickstart defaults

--- lua/custom/settings.lua
+++ lua/custom/settings.lua
@@ -2,7 +2,6 @@
 vim.opt.swapfile = false -- creates a swapfile
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
 vim.opt.jumpoptions = 'stack,view' -- preserve the jump position on the screen (if I jumped from "zb" position, I should go back there, instead of to "zz")
-vim.opt.cmdheight = 0
 
 vim.opt.spelllang:append 'cjk' -- disable spellchecking for asian characters (VIM algorithm does not support it)
 
