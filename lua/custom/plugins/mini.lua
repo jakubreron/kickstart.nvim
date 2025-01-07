50cdfdcd4802c1f33844243853ad660c44a2fd3b
Jakub Reroń <kubareron@gmail.com>
2025-01-07 13:24:36 +0900

    config: comment-out mini.statusline

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -88,16 +88,16 @@ return {
         { 'gss', desc = '[s]wap current line', icon = '' },
       }
 
-      local statusline = require 'mini.statusline'
-      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- local statusline = require 'mini.statusline'
+      -- statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- statusline.section_location = function()
+      --   return '%2l:%-2v'
+      -- end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
