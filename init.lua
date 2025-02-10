418810f083e218c7c6cd00adff7617a49957a320
Jakub Reroń <kubareron@gmail.com>
2025-02-10 14:54:05 +0900

    refactor(cmp): switch from nvim-cmp to blink

--- init.lua
+++ init.lua
@@ -203,15 +203,42 @@ require('lazy').setup {
           },
         },
       }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
+
+      { 'williamboman/mason-lspconfig.nvim' },
+      { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+
+      {
+        'saghen/blink.cmp',
+        dependencies = {
+          'rafamadriz/friendly-snippets',
+        },
+        version = '*',
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+          keymap = { preset = 'default' },
+          appearance = {
+            use_nvim_cmp_as_default = true,
+            nerd_font_variant = 'mono',
+          },
+          sources = {
+            default = { 'lsp', 'path', 'snippets', 'buffer' },
+          },
+          completion = {
+            documentation = {
+              auto_show = true,
+              auto_show_delay_ms = 0,
+            },
+          },
+          signature = { enabled = true },
+        },
+        opts_extend = { 'sources.default' },
+      },
 
       {
         'j-hui/fidget.nvim',
         config = true,
       },
-
-      'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
       vim.api.nvim_create_autocmd('LspAttach', {
@@ -260,12 +287,6 @@ require('lazy').setup {
               end,
             })
           end
-
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>lh', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, 'inlay [h]ints toggle')
-          end
         end,
       })
 
@@ -275,8 +296,6 @@ require('lazy').setup {
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
       end
 
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
       local servers = {
         lua_ls = {
           server_capabilities = {
@@ -401,7 +420,10 @@ require('lazy').setup {
         -- NOTE: 2 in 1, linters & formatters
         'markdownlint', -- markdown, vimwiki
       })
+
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       require('mason-lspconfig').setup {
         handlers = {
@@ -491,90 +513,6 @@ require('lazy').setup {
           json = { 'prettier' }, -- NOTE: nice to have prettierd, but it creates bugs in japanese characters
         },
       }
-    end,
-  },
-
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
-      },
-      'saadparwaiz1/cmp_luasnip',
-
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
-    },
-    config = function()
-      local cmp = require 'cmp'
-      local luasnip = require 'luasnip'
-      luasnip.config.setup {}
-
-      local completeopt = 'menu,menuone,noinsert,preview'
-
-      vim.opt.completeopt = completeopt
-      vim.opt.pumheight = 20 -- completion menu height
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = completeopt },
-
-        mapping = cmp.mapping.preset.insert {
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-u>'] = cmp.mapping.scroll_docs(4),
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<C-Space>'] = cmp.mapping.complete {},
-
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        },
-        sources = {
-          {
-            name = 'lazydev',
-            group_index = 0,
-          },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'path' },
-          { name = 'buffer' },
-        },
-      }
-
-      for _, ft_path in ipairs(vim.api.nvim_get_runtime_file('lua/custom/snippets/*.lua', true)) do
-        loadfile(ft_path)()
-      end
     end,
   },
 
