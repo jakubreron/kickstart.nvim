436f2180f34ed6688cfe7afc4626042be7fb3302
Jakub Reroń <kubareron@gmail.com>
2025-03-28 15:56:42 +0900

    config(diagnostics): remove signs

--- init.lua
+++ init.lua
@@ -415,16 +415,6 @@ require('lazy').setup {
         end,
       })
 
-      local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-      for type, icon in pairs(signs) do
-        local hl = 'DiagnosticSign' .. type
-        vim.fn.sign_define(hl, {
-          text = icon,
-          texthl = hl,
-          numhl = hl,
-        })
-      end
-
       local servers = {
         lua_ls = {
           server_capabilities = {
