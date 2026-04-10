b087aebb739341849f30168ea76a1d1db69f66be
Jakub Reroń <kubareron@gmail.com>
2026-04-10 20:27:46 +0900

    cleanup: ftplugin

--- after/ftplugin/typescript.lua
+++
@@ -1,3 +0,0 @@
-local javascript_and_typescript_mappings = require 'custom.ftplugin-keymaps.javascript-and-typescript'
-
-javascript_and_typescript_mappings.config(vim.api.nvim_get_current_buf())
--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -30,3 +30,38 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'markdown', 'text', 'vimwiki' },
   callback = function() vim.opt_local.spell = true end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'javascript', 'typescript', 'typescriptreact' },
+  callback = function(event)
+    local bufnr = event.buf
+    local set_keymap = function(keybind, command, desc) vim.keymap.set('n', keybind, command, { buffer = bufnr, desc = desc }) end
+
+    set_keymap(']u', function() require('neotest').jump.next { status = 'failed' } end, 'next failed [u]nit test')
+    set_keymap('[u', function() require('neotest').jump.prev { status = 'failed' } end, 'previous failed [u]nit test')
+
+    set_keymap('<leader>ur', function()
+      require('neotest').output_panel.clear()
+      require('neotest').run.run()
+    end, '[r]un nearest')
+    set_keymap('<leader>uf', function()
+      require('neotest').output_panel.clear()
+      require('neotest').run.run(vim.fn.expand '%')
+    end, 'run [f]ile')
+    set_keymap('<leader>ul', function()
+      require('neotest').output_panel.clear()
+      require('neotest').run.run_last()
+    end, 'run [l]ast')
+    set_keymap('<leader>uc', function()
+      require('neotest').summary.toggle()
+      require('neotest').output_panel.toggle()
+    end, '[c]ombo: summary tree + output panel')
+
+    set_keymap('<leader>us', function() require('neotest').run.stop() end, '[s]top')
+    set_keymap('<leader>ut', function() require('neotest').summary.toggle() end, 'summary [t]ree')
+    set_keymap('<leader>uw', function() require('neotest').run.run { jestCommand = 'jest --watch ' } end, '[w]atch')
+    set_keymap('<leader>ua', function() require('neotest').run.attach() end, '[a]ttach')
+    set_keymap('<leader>up', function() require('neotest').output_panel.toggle() end, '[p]anel toggle')
+    set_keymap('<leader>uo', function() require('neotest').output.open { enter = true } end, '[o]utput')
+  end,
+})
