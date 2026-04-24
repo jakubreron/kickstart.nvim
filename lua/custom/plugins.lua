f3d10721982c153cc58a0e154160701e1cd241fb
Jakub Reroń <kubareron@gmail.com>
2026-04-24 16:36:14 +0900

    config: remove save on tmux pane switch

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -274,7 +274,6 @@ return {
     init = function()
       vim.g.tmux_navigator_no_wrap = 1
       vim.g.tmux_navigator_disable_when_zoomed = 1
-      vim.g.tmux_navigator_save_on_switch = 2
     end,
   },
 
