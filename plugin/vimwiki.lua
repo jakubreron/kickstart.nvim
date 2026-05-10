3b8d84971c87c9eadf0d79f3d1a1429afc99f061
Jakub Reroń <kubareron@gmail.com>
2026-05-10 14:19:42 +0900

    refactor: simplify vimwiki

--- plugin/vimwiki.lua
+++ plugin/vimwiki.lua
@@ -1,4 +1,4 @@
--- 1. Configuration (Must be set BEFORE loading the plugin)
+-- Must be set BEFORE loading the plugin
 vim.g.vimwiki_list = {
   {
     path = vim.fn.expand '$VIMWIKI_DIR',
@@ -11,5 +11,23 @@ vim.pack.add { 'https://github.com/vimwiki/vimwiki' }
 
 vim.api.nvim_create_autocmd('BufNewFile', {
   pattern = vim.fs.joinpath(vim.fn.expand '$VIMWIKI_DIR', 'diary', '*.md'),
-  callback = function(event) vim.cmd('silent 0r !~/.local/bin/generate-vimwiki-diary-template ' .. vim.fn.shellescape(event.file)) end,
+  callback = function(event)
+    local filename = vim.fn.fnamemodify(event.file, ':t:r')
+    local y, m, d = filename:match '(%d%d%d%d)-(%d%d)-(%d%d)'
+
+    -- ISO week number calculation via Lua
+    local week_num = os.date('%V', os.time { year = y, month = m, day = d })
+
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {
+      '# ' .. filename .. ', Week ' .. week_num,
+      '',
+      '## To do',
+      '',
+      '* [ ] ',
+      '',
+      '## Notes',
+      '',
+      '* ...',
+    })
+  end,
 })
